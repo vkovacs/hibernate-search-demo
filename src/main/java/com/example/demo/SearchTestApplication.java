949884c98afb7796c80c1f36e02bb528ebe3758a
Viktor Kovacs <viktor.kovacs@siemens.com>
2022-11-15 12:10:33 +0100

    rename class

--- src/main/java/com/example/demo/SearchTestApplication.java
+++ src/main/java/com/example/demo/SearchTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SearchtestApplication {
+public class SearchTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SearchtestApplication.class, args);
+		SpringApplication.run(SearchTestApplication.class, args);
 	}
 
 }
